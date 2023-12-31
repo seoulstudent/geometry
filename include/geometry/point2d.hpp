4957b83acedb1106e386d75b3fd7120d2295d4d8
seoulstudent <gingtom2@naver.com>
2023-12-31 22:56:27 +0900

    Added basic description for point2d.hpp

---
+++ include/geometry/point2d.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file geometyr/point2d.hpp
+ * @author seoulstudent (gingtom2@naver.com)
+ * @brief Point class declaration with 2-dimension
+ * @version 1.0.0
+ * @date 2023-12-31 * 
+ * @copyright Copyright (c) 2023
+ */
+
+ // Copyright (c) 2023 Programmers, All Rights Reserved.
+ // Authors : Seoulstudent
+
+#ifndef SEOULSTUDENT_GEOMETRY_POINT_2_HPP_
+#define SEOULSTUDENT_GEOMETRY_POINT_2_HPP_
+
+#endif
